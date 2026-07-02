45c12188057fe52d1d50deb2455fbdd226ebdeea
darkBenky <matus.benky@gmail.com>
2026-07-03 00:16:27 +0200

    feat: add initial implementation of expression tokenizer and generator

---
+++ defines.py
@@ -0,0 +1,4 @@
+# SPECIAL_N are not used currently, but are reserved for future use and model can use it for internal reasoning
+VOCAB = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", "%", "=", "EMPTY", "SPECIAL_1", "SPECIAL_2", "SPECIAL_3", "SPECIAL_4", "SPECIAL_5", "SPECIAL_6", "SPECIAL_7", "SPECIAL_8", "SPECIAL_9", "SPECIAL_10"]
+OPERATORS = ["+", "-", "*", "/", "%"]
+INPUT_SIZE = 64
---
+++ generateData.py
@@ -0,0 +1,40 @@
+from defines import VOCAB, INPUT_SIZE, OPERATORS
+import numpy as np
+
+def tokenizeExpression(exp: str):
+    if len(exp) > INPUT_SIZE:
+        raise ValueError(f"Expression length exceeds maximum input size of {INPUT_SIZE}.")
+
+    embedding = np.zeros((INPUT_SIZE, len(VOCAB)))
+    # one hot encode the expression
+    for i, char in enumerate(exp):
+        if char not in VOCAB:
+            raise ValueError(f"Character '{char}' not in vocabulary.")
+        embedding[i, VOCAB.index(char)] = 1
+    
+    for i in range(len(exp), INPUT_SIZE):
+        embedding[i, VOCAB.index("EMPTY")] = 1
+
+    return embedding
+
+def generateExpression():
+    numberOfTokens = np.random.randint(3, INPUT_SIZE // 2)
+    probabilityOfOperator = 0.3
+    expression = ""
+    for i in range(numberOfTokens):
+        if np.random.rand() < probabilityOfOperator and i > 0 and i < numberOfTokens - 1 and expression[-1] not in OPERATORS:  # Ensure operators are not at the start or end
+            expression += np.random.choice(OPERATORS)
+        else:
+            expression += str(np.random.randint(0, 10))
+    result = str(int(eval(expression)))
+    expression += "="
+    return expression, result
+
+
+if __name__ == "__main__":
+    # Example usage
+    expression = "1+2+3+5=8"
+    embedding = tokenizeExpression(expression)
+    print(embedding)
+    expression, result = generateExpression()
+    print(f"Generated Expression: {expression}, Result: {result}")
